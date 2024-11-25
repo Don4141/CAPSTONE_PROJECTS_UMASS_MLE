bd3b1c5387194b295e7e6ee3fb12d43352f5c2a2
Samuel Adjei <69225920+Don4141@users.noreply.github.com>
2024-11-25 17:32:11 -0500

    Update UpdatedFetchUniprotIds.py

--- UpdatedFetchUniprotIds.py
+++ UpdatedFetchUniprotIds.py
@@ -1,11 +1,11 @@
 import requests
 
-def fetch_uniprot_info(protein_name):
+def fetch_uniprot_info(uniqGene_name):
     """Fetch the UniProt accession number and entry name for a given protein or gene name.
        The script read gene names from input_genes.txt, fetch their corresponding Uniprot IDs and entry name using UniProt REST API,
        and output the results to uniprot_results2.txt.
     """
-    url = f"https://rest.uniprot.org/uniprotkb/search?query=gene_exact:{protein_name} AND reviewed:true&fields=accession,id&format=tsv" #Constructs a URL to query Uniprot database with given gene name and fetches Uniprot ID if available.
+    url = f"https://rest.uniprot.org/uniprotkb/search?query=gene_exact:{UniqGene_name} AND reviewed:true&fields=accession,id&format=tsv" #Constructs a URL to query Uniprot database with given gene name and fetches Uniprot ID if available.
     response = requests.get(url)
     if response.status_code == 200:
         lines = response.text.split('\n')
@@ -13,9 +13,9 @@ def fetch_uniprot_info(protein_name):
             data = lines[1].split('\t')
             return data[0].strip(), data[1].strip()  # Return both the UniProt accession and the entry name
         else:
-            return None, f"No results found for {protein_name}."
+            return None, f"No results found for {UniqGene_name}."
     else:
-        return None, f"Failed to fetch data for {protein_name}: Status code {response.status_code}, Response: {response.text}"
+        return None, f"Failed to fetch data for {UniqGene_name}: Status code {response.status_code}, Response: {response.text}"
 
 def process_gene_names(input_file, output_file):
     with open(input_file, 'r') as file:
@@ -34,6 +34,6 @@ def process_gene_names(input_file, output_file):
 
 if __name__ == "__main__":
     input_file = "./input_genes.txt"  # File where gene names are listed
-    output_file = "./uniprot_results2.txt"  # File to output the UniProt IDs and entry names
+    output_file = "./uniprot_results.txt"  # File to output the UniProt IDs and entry names
     process_gene_names(input_file, output_file)
     print(f"Results have been written to {output_file}.")
