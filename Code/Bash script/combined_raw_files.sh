83572162194852a40a1c549df3b0ae2c97cb4f53
Samuel Adjei <69225920+Don4141@users.noreply.github.com>
2024-12-18 18:53:25 -0500

    Update combined_raw_files.sh

--- Code/Bash script/combined_raw_files.sh
+++ Code/Bash script/combined_raw_files.sh
@@ -7,7 +7,7 @@ input_dir="./BuildModel"
 output_file="./combined_output2.csv"
 
 # Add header to the output file
-echo "Pdb,Total_energy,Backbone_Hbond,Sidechain_Hbond,Van_der_Waals,Electrostatics,Solvation_Polar,Solvation_Hydrophobic,Van_der_Waals_clashes,Entropy_sidechain,Entropy_mainchain,sloop_entropy,mloop_entropy,Cis_bond,Torsional_clash,Backbone_clash,Helix_dipole,Water_bridge,Disulfide,Electrostatic_kon,Partial_covalent_bonds,Energy_Ionisation,Entropy_Complex" > "$output_file"
+echo "Pdb,Total_energy,Backbone_Hbond,Sidechain_Hbond,Van_der_Waals,Electrostatics,Solvation_Polar,Solvation_hydrophobic,Van_der_Waals_clashes,Entropy_sidechain,Entropy_mainchain,sloop_entropy,mloop_entropy,Cis_bond,Torsional_clash,Backbone_clash,Helix_dipole,Water_bridge,Disulfide,Electrostatic_kon,Partial_covalent_bonds,Energy_Ionisation,Entropy_complex" > "$output_file"
 
 # Iterate through each Raw_*.fxout file in the directory
 for file in "$input_dir"/Raw_*.fxout; do
